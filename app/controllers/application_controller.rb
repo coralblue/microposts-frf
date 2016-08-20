4c5701b07dfea653ecbe94dbb01f33c040622480
coral <izjpbd@gmail.com>
2016-08-20 12:50:47 +0000

    add followings/followers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-    else
-      render 'new'
-    end
-  end
+  include SessionsHelper
 
   private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
